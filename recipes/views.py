16f6ac7479d053e2432181ef0a23dd3be84599ff
kevin <kevinup13@gmail.com>
2024-10-09 18:13:56 -0400

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('Area Sobre - 02')
-
