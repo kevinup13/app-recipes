f2ed81d46ae0d39f94050377de2c2430a220b417
kevin <kevinup13@gmail.com>
2024-10-18 00:25:10 -0400

    Configuacao de pastas statics e ajuste de css

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+# Create your views here.
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
